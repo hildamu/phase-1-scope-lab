accbdfb9cd3e2431ef413ac1f775e3c10ded549b
hildamu <mukamihilda437@gmail.com>
2024-03-21 16:04:15 +0300

    include new files

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = 'hilda';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+
+}
+
+function setBestCustomer(){
+    let bestCustomer = 'not bob'
+    console.log ('setbestCustomer');
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'maybe hilda';
+
+}
